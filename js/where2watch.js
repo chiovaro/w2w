b0b69f49568853a070728a5b5b6a5d5a167cfc73
Zach Chiovaro <chiovaro@gmail.com>
2014-08-10 18:21:49 -0400

    w2w layout

---
+++ js/where2watch.js
@@ -0,0 +1,28 @@
+$(function() {
+  // Hide search wrap by default;
+  $(".search-wrap").hide();
+
+  $(".toggle-search").on("click", function(e) {
+    // Prevent default link behavior
+    e.preventDefault();
+
+    // Stop propagation
+    e.stopPropagation();
+
+    // Toggle search-wrap
+    $(".search-wrap").slideToggle(500, function() {
+      // Focus on the search bar
+      // When animation is complete
+      $("#search-bar").focus();
+    });
+  });
+
+  // Close the search bar if user clicks anywhere
+  $(document).click(function(e) {
+    var searchWrap = $(".search-wrap");
+
+    if (!searchWrap.is(e.target) && searchWrap.has(e.target).length === 0) {
+      searchWrap.slideUp(500);
+    }
+  });
+});
